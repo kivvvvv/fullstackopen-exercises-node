2c3085cfb0388b84ce0634025a2e2e31471b9b1a
Sirapop Iam-o-pas <sirapop@mysocialmotion.org>
2020-07-06 09:31:07 +0700

    add part3::3.11: phonebook backend

---
+++ build/precache-manifest.96c91c93e963aa73a723068493c569c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd1cf62e6ec9676e17854589c8119aa7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b9d7aed4bb20bcc7e38",
+    "url": "/static/css/main.74329bc6.chunk.css"
+  },
+  {
+    "revision": "c5cf8bd149580aa910cd",
+    "url": "/static/js/2.57bf6790.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.57bf6790.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b9d7aed4bb20bcc7e38",
+    "url": "/static/js/main.1080b89f.chunk.js"
+  },
+  {
+    "revision": "9d2b4f7262278411ffe7",
+    "url": "/static/js/runtime-main.4464e187.js"
+  }
+]);
